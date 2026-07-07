aa0cd72d76d9c925ce10cfc25fa1041d7b012c1d
yokomaru <cabochetoganlan@gmail.com>
2026-07-07 17:19:15 +0900

    RSpec関連ファイルの生成と設定を行った

--- config/application.rb
+++ config/application.rb
@@ -39,4 +39,11 @@ module Citytori
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
+
+  config.generators do |g|
+    g.test_framework :rspec,
+    fixtures: false,
+    view_specs: false,
+    routing_specs: false
+  end
 end
