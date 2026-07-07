52605ba1fb0cfd4a73be2419f028db05cd9fa7fa
yokomaru <cabochetoganlan@gmail.com>
2026-07-07 18:59:01 +0900

    config.generatorsの位置を修正

--- config/application.rb
+++ config/application.rb
@@ -38,12 +38,12 @@ module Citytori
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-  end
 
-  config.generators do |g|
-    g.test_framework :rspec,
-    fixtures: false,
-    view_specs: false,
-    routing_specs: false
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      routing_specs: false
+    end
   end
 end
