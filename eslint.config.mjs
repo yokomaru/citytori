bfd0520fd5f0db019981edcc7511289dc117da27
yokomaru <cabochetoganlan@gmail.com>
2026-07-08 10:55:07 +0900

    eslint-config-prettierを設定に反映させる

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import js from "@eslint/js";
 import globals from "globals";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["js/recommended", prettier],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
 ]);
