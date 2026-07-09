0f69ab9a43d46eda5eb9b4a773446c1192b58ce2
yokomaru <cabochetoganlan@gmail.com>
2026-07-09 17:29:32 +0900

    credentials情報を取得できなかった際のnilガードとエラーメッセージを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  google_credentials = Rails.application.credentials.google
+  if google_credentials.nil?
+    raise "Google OAuth credentials not configured. Run `bin/rails credentials:edit --environment #{Rails.env}` and add a `google` section with `client_id` and `client_secret`."
+  end
+
   provider :google_oauth2,
-          Rails.application.credentials.google[:client_id],
-          Rails.application.credentials.google[:client_secret]
+          google_credentials[:client_id],
+          google_credentials[:client_secret]
 end
+
 OmniAuth.config.request_validation_phase = OmniAuth::AuthenticityTokenProtection.new(key: :_csrf_token)
 
 OmniAuth.config.on_failure = lambda do |env|
