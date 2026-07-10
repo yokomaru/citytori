c22bf7f2c53ada5181095f866821fd51e23b7388
yokomaru <cabochetoganlan@gmail.com>
2026-07-10 17:01:21 +0900

    WordChainWalkにStep関連のメソッドを追加
    - 最新のStepを取得する処理
    - 探している文字を取得する処理

--- app/models/word_chain_walk.rb
+++ app/models/word_chain_walk.rb
@@ -25,6 +25,10 @@ class WordChainWalk < ApplicationRecord
     finished_at.present?
   end
 
+  def latest_step
+    word_chain_walk_steps.order(:id).last
+  end
+
   def elapsed_seconds
     return nil if started_at.nil?
 
@@ -33,6 +37,12 @@ class WordChainWalk < ApplicationRecord
     else
       Time.zone.now - started_at
     end
+  end
+
+  def target_char
+    return start_char if latest_step.nil?
+
+    latest_step.word[-1] # 本当は正規化必要
   end
 
   private
