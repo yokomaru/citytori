7f796c8327d932d2eb4d9f22c20be42bc5b9b21d
yokomaru <cabochetoganlan@gmail.com>
2026-07-24 18:45:29 +0900

    ステップの登録動線のテストを修正
    - モーダルからステップの登録できること
    - 登録したデータが一覧に反映されていること
    - 登録後にモーダルが閉じること

--- spec/system/word_chain_walks_spec.rb
+++ spec/system/word_chain_walks_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'WordChainWalks', type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by(:selenium_chrome_headless)
   end
 
   let(:user) { FactoryBot.create(:user) }
@@ -36,19 +36,33 @@ RSpec.describe 'WordChainWalks', type: :system do
     visit '/auth/google_oauth2/callback'
   end
 
-  scenario '画像を選択してStepを登録できる' do
+  scenario 'モーダルからステップを登録できること' do
     log_in(user)
 
     visit word_chain_walk_path(word_chain_walk)
     click_button '写真を撮る'
 
+    expect(page).to have_css(
+      'dialog[data-modal-target="modal"][open]'
+    )
+
     fill_in '見つけた言葉', with: 'りんご'
     fill_in 'メモ', with: 'メモ'
     attach_file '写真', Rails.root.join('spec/fixtures/files/480x320.png')
 
-    expect do
-      click_button '登録する'
+    click_button '登録する'
+
+    within '#word_chain_walk_steps' do
       expect(page).to have_content('りんご')
-    end.to change(WordChainWalkStep, :count).by(1)
+    end
+
+    expect(page).to have_css(
+      '#word_chain_walk_target',
+      text: 'ご'
+    )
+
+    expect(page).to have_no_css(
+      'dialog[data-modal-target="modal"][open]'
+    )
   end
 end
