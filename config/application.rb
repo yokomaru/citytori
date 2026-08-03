3b8eb0bd9de005485ac6cd16032e491ae08a9012
yokomaru <cabochetoganlan@gmail.com>
2026-08-03 18:15:03 +0900

    ja.ymlファイルを読み込む設定とファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,6 @@ module Citytori
 
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
