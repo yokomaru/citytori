af4e9635bc2f8195551b5f3f9497aae1b905fd2d
yokomaru <cabochetoganlan@gmail.com>
2026-08-12 19:24:44 +0900

    home controllerからcurrent_userの分岐を削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate, only: :show
 
-  def show
-    if current_user
-      redirect_to word_chain_walks_path
-    end
-  end
+  def show;end
 end
