10b80a1b2c6f2a02b9e4ad6101fe1311ddf03d73
yokomaru <cabochetoganlan@gmail.com>
2026-08-13 23:31:54 +0900

    進行中の散歩を複数作れないように部分インデックスを使用してユーザーと散歩のユニーク制約を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_10_072658) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_13_141425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_10_072658) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_word_chain_walks_on_user_id"
+    t.index ["user_id"], name: "index_word_chain_walks_on_user_id_active", unique: true, where: "(finished_at IS NULL)"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
