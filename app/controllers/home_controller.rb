5658249b332ae4eaa5e115d63fe2153a2f188047
yokomaru <cabochetoganlan@gmail.com>
2026-08-14 10:10:37 +0900

    今のしりとり散歩の一覧画面に書いてある処理をセクション分けしてホームに移した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate, only: :show
 
-  def show;end
+  def show
+    if logged_in?
+      @active_word_chain_walks = current_user.word_chain_walks.active.includes(:word_chain_walk_steps).order(id: :desc)
+      @finished_word_chain_walks = current_user.word_chain_walks.finished.includes(:word_chain_walk_steps).order(id: :desc)
+    end
+  end
 end
