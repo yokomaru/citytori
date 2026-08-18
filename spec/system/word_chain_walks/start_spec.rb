13bec4e5ea9d8594c7e0ac500633df904c018fa3
yokomaru <cabochetoganlan@gmail.com>
2026-08-18 14:45:32 +0900

    位置情報APIで位置情報が取得できなかった場合のテストを追加した
    - https://github.com/yokomaru/citytori/pull/279#pullrequestreview-4950769380
    のNitPicksを対応

--- spec/system/word_chain_walks/start_spec.rb
+++ spec/system/word_chain_walks/start_spec.rb
@@ -33,11 +33,57 @@ RSpec.describe 'Start', type: :system do
       click_button '始める'
 
       expect(page).to have_content('しりとり散歩中')
+      expect(user.word_chain_walks.count).to eq(1)
+
+      created_word_chain_walk = user.word_chain_walks.order(:id).last
+      expect(created_word_chain_walk.start_latitude).to  eq(35.6586)
+      expect(created_word_chain_walk.start_longitude).to eq(139.7454)
+    ensure
+      page.driver.browser.execute_cdp('Emulation.clearGeolocationOverride')
+    end
+  end
+
+  scenario 'しりとり散歩開始時に開始地点の位置情報の取得ができなかった場合、そのまま進める場合は開始地点の位置情報はnilで保存されること' do
+    log_in(user)
+
+    visit root_path
+
+    begin
+      set_failure_browser_geolocation
+
+      accept_confirm("位置情報を取得できませんでした。このまま開始の位置情報なしで進めますか？") do
+        click_button '始める'
+      end
+
+      expect(page).to have_content('しりとり散歩中')
+      expect(user.word_chain_walks.count).to eq(1)
 
       created_word_chain_walk = user.word_chain_walks.order(:id).last
 
-      expect(created_word_chain_walk.start_latitude).to  eq(35.6586)
-      expect(created_word_chain_walk.start_longitude).to eq(139.7454)
+      expect(created_word_chain_walk.start_latitude).to be_nil
+      expect(created_word_chain_walk.start_longitude).to be_nil
+    ensure
+      page.driver.browser.execute_cdp('Emulation.clearGeolocationOverride')
+    end
+  end
+
+  scenario 'しりとり散歩開始時に開始地点の位置情報の取得ができなかった場合、進めない場合はしりとり散歩自体が作成されないこと' do
+    log_in(user)
+
+    visit root_path
+
+    begin
+      set_failure_browser_geolocation
+
+
+      accept_alert("電波状況や権限を見直して再度開始ボタンを押してください") do
+        dismiss_confirm("位置情報を取得できませんでした。このまま開始の位置情報なしで進めますか？") do
+          click_button '始める'
+        end
+      end
+
+
+      expect(user.word_chain_walks.count).to eq(0)
     ensure
       page.driver.browser.execute_cdp('Emulation.clearGeolocationOverride')
     end
@@ -49,6 +95,12 @@ RSpec.describe 'Start', type: :system do
       latitude:,
       longitude:,
       accuracy:
+    )
+  end
+
+  def set_failure_browser_geolocation
+    page.driver.browser.execute_cdp(
+      'Emulation.setGeolocationOverride',
     )
   end
 end
