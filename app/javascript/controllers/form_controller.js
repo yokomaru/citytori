d752e8eb347c58f5b9ad0efa50f2f59033b2ff68
yokomaru <cabochetoganlan@gmail.com>
2026-08-19 18:15:59 +0900

    位置情報を取得処理と連携するため完了ボタンを押した時のアラートをStimulusに移動した

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -5,4 +5,10 @@ export default class extends Controller {
   submit() {
     this.element.requestSubmit();
   }
+
+  finishConfirm(event) {
+    if (!window.confirm("しりとり散歩をおしまいにしますか？")) {
+      event.stopImmediatePropagation();
+    }
+  }
 }
