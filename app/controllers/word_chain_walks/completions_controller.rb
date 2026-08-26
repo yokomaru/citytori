fa8640b594cef28d5d316451005a7a8830eee03a
yokomaru <cabochetoganlan@gmail.com>
2026-08-26 15:40:17 +0900

    不要なコールバックを削除した

--- app/controllers/word_chain_walks/completions_controller.rb
+++ app/controllers/word_chain_walks/completions_controller.rb
@@ -1,9 +1,8 @@
 class WordChainWalks::CompletionsController < ApplicationController
   before_action :authenticate
-  before_action :set_word_chain_walk
 
   def show
-    return if @word_chain_walk.finished?
+    @word_chain_walk = current_user.word_chain_walks.preload(:word_chain_walk_steps).find(params[:word_chain_walk_id])
 
     unless @word_chain_walk.finished?
       redirect_to word_chain_walk_path(@word_chain_walk), alert: "まだ完了していません", status: :see_other
@@ -16,16 +15,11 @@ class WordChainWalks::CompletionsController < ApplicationController
   end
 
   def update
+    @word_chain_walk = current_user.word_chain_walks.preload(:word_chain_walk_steps).find(params[:word_chain_walk_id])
     finished_now = @word_chain_walk.finish
 
     redirect_to word_chain_walk_completion_path(@word_chain_walk),
                 notice: finished_now ? "しりとり散歩が完了しました" : "すでに散歩は完了しています",
                 status: :see_other
-  end
-
-  private
-
-  def set_word_chain_walk
-    @word_chain_walk =current_user.word_chain_walks.preload(:word_chain_walk_steps).find(params[:word_chain_walk_id])
   end
 end
