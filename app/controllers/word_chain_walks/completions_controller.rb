4efe43e542eac45948097bd5892b05da6daaf8ab
yokomaru <cabochetoganlan@gmail.com>
2026-08-26 17:42:10 +0900

    不要なpreloadやクエリを呼ぶ可能性のある箇所を修正した
    https://github.com/yokomaru/citytori/pull/301#discussion_r3860842180

--- app/controllers/word_chain_walks/completions_controller.rb
+++ app/controllers/word_chain_walks/completions_controller.rb
@@ -2,20 +2,20 @@ class WordChainWalks::CompletionsController < ApplicationController
   before_action :authenticate
 
   def show
-    @word_chain_walk = current_user.word_chain_walks.preload(:word_chain_walk_steps).find(params[:word_chain_walk_id])
+    @word_chain_walk = current_user.word_chain_walks.find(params[:word_chain_walk_id])
 
     unless @word_chain_walk.finished?
       redirect_to word_chain_walk_path(@word_chain_walk), alert: "まだ完了していません", status: :see_other
       return
     end
 
-    @word_chain_walk_steps = @word_chain_walk.word_chain_walk_steps.order(:id)
+    @word_chain_walk_steps = @word_chain_walk.word_chain_walk_steps.with_attached_image.order(:id)
     @first_step = @word_chain_walk_steps.first
     @last_step = @word_chain_walk_steps.last
   end
 
   def update
-    @word_chain_walk = current_user.word_chain_walks.preload(:word_chain_walk_steps).find(params[:word_chain_walk_id])
+    @word_chain_walk = current_user.word_chain_walks.find(params[:word_chain_walk_id])
     finished_now = @word_chain_walk.finish
 
     redirect_to word_chain_walk_completion_path(@word_chain_walk),
