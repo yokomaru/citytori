a527b7b67f4c2b091981decfdf4238ea0f7d44b3
yokomaru <cabochetoganlan@gmail.com>
2026-08-31 09:05:51 +0900

    位置情報の配列をハッシュ化して画像も渡すようにした
    - showにはlocationsは不要だったので削除した

--- app/controllers/word_chain_walks_controller.rb
+++ app/controllers/word_chain_walks_controller.rb
@@ -8,14 +8,21 @@ class WordChainWalksController < ApplicationController
   def show
     @word_chain_walk = current_user.word_chain_walks.find(params[:id])
     @word_chain_walk_steps = @word_chain_walk.word_chain_walk_steps.order(id: :desc)
-    @locations = @word_chain_walk.word_chain_walk_steps.where.not(latitude: nil).where.not(longitude: nil).order(id: :desc).pluck(:latitude, :longitude)
     @word_chain_walk_step = @word_chain_walk.word_chain_walk_steps.build
   end
 
   def map
     @word_chain_walk = current_user.word_chain_walks.finished.find(params[:id])
     @word_chain_walk_steps = @word_chain_walk.word_chain_walk_steps.order(id: :desc)
-    @locations = @word_chain_walk.word_chain_walk_steps.where.not(latitude: nil).where.not(longitude: nil).order(id: :desc).pluck(:latitude, :longitude)
+    @locations = @word_chain_walk_steps.filter_map do |step|
+      next unless step.latitude.present? && step.longitude.present?
+      {
+        latitude: step.latitude,
+        longitude: step.longitude,
+        image: url_for(step.image),
+        id: step.id
+      }
+    end
   end
 
   def create
