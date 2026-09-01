7e2ad7d8ea73a770916a90c79c9ef322bb284dd6
yokomaru <cabochetoganlan@gmail.com>
2026-09-01 17:23:17 +0900

    重複していたcreateをletにまとめた

--- spec/system/word_chain_walks/finished_walks_spec.rb
+++ spec/system/word_chain_walks/finished_walks_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'FinishedWalks', type: :system do
 
   let(:user) { FactoryBot.create(:user) }
   let(:word_chain_walk) { FactoryBot.create(:word_chain_walk, user: user, start_char: 'り') }
+  let!(:word_chain_walk_step) { FactoryBot.create(:word_chain_walk_step, :with_image, word_chain_walk: word_chain_walk, word: 'りんご', latitude: 35.6586, longitude: 139.7454) }
 
   def log_in(user)
     OmniAuth.config.test_mode = true
@@ -26,14 +27,7 @@ RSpec.describe 'FinishedWalks', type: :system do
   scenario '完了済みの散歩でタイムラインと地図を切り替えられる' do
     log_in(user)
 
-    FactoryBot.create(
-      :word_chain_walk_step,
-      :with_image,
-      word_chain_walk: word_chain_walk,
-      word: 'りんご',
-      latitude: 35.6586,
-      longitude: 139.7454
-    )
+    word_chain_walk_step
 
     word_chain_walk.update!(finished_at: Time.current)
 
@@ -51,21 +45,14 @@ RSpec.describe 'FinishedWalks', type: :system do
   scenario '地図のマーカーをクリックすると対応する記録の情報が表示される' do
     log_in(user)
 
-    step = FactoryBot.create(
-      :word_chain_walk_step,
-      :with_image,
-      word_chain_walk: word_chain_walk,
-      word: 'りんご',
-      latitude: 35.6586,
-      longitude: 139.7454
-    )
+    word_chain_walk_step
 
     word_chain_walk.update!(finished_at: Time.current)
 
     visit map_word_chain_walk_path(word_chain_walk)
 
     within '#walk-map-container' do
-      find("a[href='#{word_chain_walk_word_chain_walk_step_path(word_chain_walk, step)}']").click
+      find("a[href='#{word_chain_walk_word_chain_walk_step_path(word_chain_walk, word_chain_walk_step)}']").click
     end
 
     within '#selected_walk_step' do
@@ -76,30 +63,23 @@ RSpec.describe 'FinishedWalks', type: :system do
   scenario '地図のピンを押して表示されたカードから記録の詳細画面に移動できる' do
     log_in(user)
 
-    step = FactoryBot.create(
-      :word_chain_walk_step,
-      :with_image,
-      word_chain_walk: word_chain_walk,
-      word: 'りんご',
-      latitude: 35.6586,
-      longitude: 139.7454
-    )
+    word_chain_walk_step
 
     word_chain_walk.update!(finished_at: Time.current)
 
     visit map_word_chain_walk_path(word_chain_walk)
 
-    step_path = word_chain_walk_word_chain_walk_step_path(word_chain_walk, step)
+    word_chain_walk_step_path = word_chain_walk_word_chain_walk_step_path(word_chain_walk, word_chain_walk_step)
 
     within '#walk-map-container' do
-      find("a[href='#{step_path}']").click
+      find("a[href='#{word_chain_walk_step_path}']").click
     end
 
     within '#selected_walk_step' do
       click_link 'りんご'
     end
 
-    expect(page).to have_current_path(step_path)
+    expect(page).to have_current_path(word_chain_walk_step_path)
     expect(page).to have_content('りんご')
   end
 end
